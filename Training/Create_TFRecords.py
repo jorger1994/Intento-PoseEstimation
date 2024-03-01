7eeeade8c2b6a94ab61e5ceaba2750cf5f2d5a3f
jorger94 <jorgecruzgalarraga@hotmail.com>
2024-03-02 00:50:12 +0100

    updated filepaths on the Create_TFRecords file

--- Training/Create_TFRecords.py
+++ Training/Create_TFRecords.py
@@ -1,9 +1,9 @@
 import os
 import TFRecord_Functions
 
-label_file = 'C:/Users/jorge/Documents/Python/Intento PoseEstimation/Test Pose Labels_17Keypoints_json.json'
-image_folder = 'C:/Users/jorge/Documents/Python/Intento PoseEstimation/Images/'
+label_file = 'C:/Users/jorge/Documents/Python/Intento PoseEstimation2/Test Pose Labels_17Keypoints_json.json'
+image_folder = 'C:/Users/jorge/Documents/Python/Intento PoseEstimation2/Images/'
 record_name = 'two_images_17keypoints_trial3'
-destination_dir = 'C:/Users/jorge/Documents/Python/Intento PoseEstimation/Datasets/'
+destination_dir = 'C:/Users/jorge/Documents/Python/Intento PoseEstimation2/Datasets/'
 
 TFRecord_Functions.create_records(label_file=label_file, image_folder = image_folder, record_name = record_name, destination_dir = destination_dir)
